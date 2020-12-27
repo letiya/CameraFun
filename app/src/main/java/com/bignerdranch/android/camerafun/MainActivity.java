d66d72492bd4b9243afa7aed4e8d7f90e7e207be
Belle Lee <letiya66@gmail.com>
2020-12-27 13:54:33 -0800

    Create a button to load image from Gallery to an ImageView

--- app/src/main/java/com/bignerdranch/android/camerafun/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/camerafun/MainActivity.java
@@ -1,14 +1,114 @@
 package com.bignerdranch.android.camerafun;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_PHOTO = 0;
+
+    private Button mSelectButton;
+    private ImageView mPhotoView;
+    private Button mAddEffectsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSelectButton = findViewById(R.id.photo_select_btn);
+        mSelectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PHOTO);
+            }
+        });
+
+        mPhotoView = findViewById(R.id.photo_view);
+        mAddEffectsButton = findViewById(R.id.effects_btn);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PHOTO: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                    Intent intent = new Intent(Intent.ACTION_PICK,
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, REQUEST_PHOTO);
+                } else {
+                    // Explain to the user that the feature is unavailable because
+                    // the features requires a permission that the user has denied.
+                    // At the same time, respect the user's decision. Don't link to
+                    // system settings in an effort to convince the user to change
+                    // their decision.
+                    Toast toast = Toast.makeText(this, R.string.permission_warning, Toast.LENGTH_LONG);
+                    toast.show();
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_PHOTO && data != null) {
+            Uri photoSelectUri = data.getData();
+            // Specify which fields you want your query to return values for
+            String[] queryFields = new String[]{MediaStore.Images.Media.DATA};
+            // Perform your query - the contactUri is like a "where" clause here
+            Cursor c = getContentResolver().query(photoSelectUri, queryFields, null, null, null);
+            try {
+                // Double check that you actually got results
+                if (c.getCount() == 0) {
+                    return;
+                }
+                // Pull out the first column of the first row of data - that is your suspect's name
+                c.moveToFirst();
+                String photoPath = c.getString(0);
+                updatePhotoView(photoPath);
+            } finally {
+                c.close();
+            }
+        }
+    }
+
+    private void updatePhotoView(String path) {
+        if (path == null) {
+            return;
+        }
+        File file = new File(path);
+        if (mPhotoView == null || !file.exists()) {
+            mPhotoView.setImageDrawable(null);
+        } else {
+            Bitmap bitmap = BitmapFactory.decodeFile(path);
+            mPhotoView.setImageBitmap(bitmap);
+        }
     }
 }
